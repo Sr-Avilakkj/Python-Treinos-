0278d68071d674824933c0ec0536d251cbfa28a1
Teonas <teonasnetto@gmail.com>
2024-04-19 14:40:32 -0300

    feat: create a new structure for the code

--- Cu.py
+++ Cu.py
@@ -4,22 +4,32 @@ soma = 0
 chutes = 0
 numero_sorteado = 0
 
-def menu():
-    num = int(input('— Digite um numero inteiro entre 1 e 100: '))
-    return num
 
-while True:
-    num = menu()
-    
-    if num < 1 or num > 100:
-        print('– O numero escolhido é invalido. Escolha um numero inteiro entre 1 e 100')
-        
-    else:
-        while num != numero_sorteado:
-            numero_sorteado = randint(1, 100)
-            print(numero_sorteado)
-            soma += numero_sorteado
-            chutes += 1
-        media = soma / chutes
-        print(f'•Foram necessários {chutes} chutes \n•A soma deles é igual a {soma} \n•E a sua média é igual a {media}')
-    break
+class Menu:
+    def menu(self):
+        num = int(input('— Digite um numero inteiro entre 1 e 100: '))
+        return num
+
+
+def main():
+    while True:
+        num = Menu().menu()
+
+        if num < 1 or num > 100:
+            print(
+                '– O numero escolhido é invalido. Escolha um numero inteiro entre 1 e 100')
+
+        else:
+            while num != numero_sorteado:
+                numero_sorteado = randint(1, 100)
+                print(numero_sorteado)
+                soma += numero_sorteado
+                chutes += 1
+            media = soma / chutes
+            print(
+                f'•Foram necessários {chutes} chutes \n•A soma deles é igual a {soma} \n•E a sua média é igual a {media}')
+        break
+
+
+if __name__ == '__main__':
+    main()
